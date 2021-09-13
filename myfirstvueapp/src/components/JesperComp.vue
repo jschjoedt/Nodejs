3d27db608bf718dd6a3a5d435bd53d961c501624
Jesper Schjødt <jesper.schjoedt@invixo.com>
2021-09-13 14:32:57 +0200

    New Vue project test

---
+++ myfirstvueapp/src/components/JesperComp.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="hello">
+    <h1>Jesper siger hej!</h1>
+  </div> 
+</template>
---
+++ myfirstvueapp/src/views/Jesper.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="jesper">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <JesperComp/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import JesperComp from "@/components/JesperComp.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    JesperComp,
+  },
+})
+export default class Jesper extends Vue {}
+</script>
