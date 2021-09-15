2f7344c95be4720b98c520f7e4ff94b7b16c4700
Jesper Schjødt <jesper.schjoedt@invixo.com>
2021-09-15 10:20:16 +0200

    Static Employee data used

---
+++ vue3-app/src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <p>=============== This is one mighty footer ===============</p>
+    </div>
+</template>
---
+++ vue3-app/src/components/SearchResults.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="root">
+    <p>Showing {{ filteredEmployees.length }} results for "{{ query }}"</p>
+    <ul>
+      <li v-for="e in filteredEmployees" :key="e.FirstName">
+        {{ e.FirstName }}, {{ e.LastName }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import { computed, onMounted, methods } from "vue";
+import employees from "../assets/post-data.json";
+export default {
+  props: {
+    query: String,
+  },
+  setup(props) {
+    onMounted(() => {
+      console.log("mounted");
+    });
+    // Filter employees on "FirstName" based on input query sent from "HelloWorld"
+    const filteredEmployees = computed(() => {
+      return employees.value.filter((s) =>
+        s.FirstName.toLowerCase().includes(props.query.toLowerCase())
+      );
+    });
+    return {
+      filteredEmployees,
+    };
+  },
+  methods: {
+	  sayHello() {
+		  alert("pelle");
+	  }
+  },
+};
+</script>
+
+<style scoped>
+.root {
+  width: 400px;
+  margin: 0 auto;
+}
+
+.root p {
+  text-align: right;
+  font-size: 0.7em;
+  margin: 0;
+}
+
+ul {
+  list-style: none;
+  width: 50px 0;
+  padding: 0;
+  background-color: #fafafa;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+}
+
+li {
+  text-align: left;
+  padding: 20px;
+  border-bottom: 1px solid #ddd;
+}
+
+li:nth-last-of-type(1) {
+  border-bottom: none;
+}
+</style>
